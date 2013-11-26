c54ada3fc7bd92859639552cf40e9867ae14da1d
Chisa <monochromevoice@gmail.com>
2013-11-26 15:34:05 +0100

    Update 05-grand-inventaire-obligatoire.py

--- niveau-02/chapitre-2-decouverte-tableaux/05-grand-inventaire-obligatoire.py
+++ niveau-02/chapitre-2-decouverte-tableaux/05-grand-inventaire-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 05-grand-inventaire-obligatoire.py
 # nom de l'exercice : Grand inventaire
@@ -17,3 +16,17 @@
 
 # mettre votre code ici
 
+produits = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+nbOperations = int(input())
+
+for loop in range(nbOperations):
+   numeroIngredient = int(input())
+   quantite = int(input())
+   numeroIngredient = numeroIngredient - 1
+   produits[numeroIngredient] = produits[numeroIngredient] + quantite
+   
+idProduit = 0
+for loop in range(10):
+   print(produits[idProduit])
+   idProduit = idProduit + 1
