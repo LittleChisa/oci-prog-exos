ec462d6a59cc0961e61ec8973e5a77ff35fb3c3a
Chisa <monochromevoice@gmail.com>
2013-12-03 16:22:26 +0100

    Update 08-visite-de-la-mine-obligatoire.py

--- niveau-02/chapitre-2-decouverte-tableaux/08-visite-de-la-mine-obligatoire.py
+++ niveau-02/chapitre-2-decouverte-tableaux/08-visite-de-la-mine-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 08-visite-de-la-mine-obligatoire.py
 # nom de l'exercice : Visite de la mine
@@ -17,3 +16,27 @@
 
 # mettre votre code ici
 
+nbDeplacements = int(input())
+tableau = [0] * nbDeplacements
+rang = 0
+
+for loop in range(nbDeplacements):
+   deplacement = int(input())
+   tableau[rang] = deplacement
+   rang = rang + 1
+   
+rang = nbDeplacements - 1
+for loop in range(nbDeplacements):
+   if tableau[rang] == 1:
+      print(2)
+   elif tableau [rang] == 2:
+      print(1)
+   elif tableau[rang] == 3:
+      print(3)
+   elif tableau[rang] == 4:
+      print(5)
+   elif tableau[rang] == 5:
+      print(4)
+   
+   rang = rang - 1
+     
