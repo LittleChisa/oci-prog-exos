e9a4cd32b3df0934632230b6b9f4c5d43e418c33
Chisa <monochromevoice@gmail.com>
2014-01-06 19:14:00 +0100

    Update 07-convertisseur-d-unites-obligatoire.py

--- niveau-02/chapitre-4-fonctions/07-convertisseur-d-unites-obligatoire.py
+++ niveau-02/chapitre-4-fonctions/07-convertisseur-d-unites-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 07-convertisseur-d-unites-obligatoire.py
 # nom de l'exercice : Convertisseur d'unités
@@ -17,3 +16,25 @@
 
 # mettre votre code ici
 
+nbValeurs = int(input())
+
+def metrePied():   
+   return nombre / 0.3048
+
+def grammeLivre():
+   return nombre * 0.002205
+
+def celsiusFarenh():
+   return (nombre * 1.8) + 32
+
+for loop in range(nbValeurs):
+   entier = input().split(" ")
+   nombre = float(entier[0])
+   unite = entier[1]
+   
+   if unite == "m":
+      print(metrePied(), "p")
+   elif unite ==  "g":
+      print(grammeLivre(), "l")
+   elif unite == "c":
+      print(celsiusFarenh(), "f")
