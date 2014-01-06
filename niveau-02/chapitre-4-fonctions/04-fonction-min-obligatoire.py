e6ff09e3ea8dd76d57f232f6adbdc46ff750e611
Chisa <monochromevoice@gmail.com>
2014-01-06 19:13:20 +0100

    Update 04-fonction-min-obligatoire.py

--- niveau-02/chapitre-4-fonctions/04-fonction-min-obligatoire.py
+++ niveau-02/chapitre-4-fonctions/04-fonction-min-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 04-fonction-min-obligatoire.py
 # nom de l'exercice : Fonction min
@@ -17,3 +16,23 @@
 
 # mettre votre code ici
 
+def min(entier1, entier2):
+   if entier1 < entier2:
+        return entier1
+   else:
+        return entier2
+
+liste = [0] * 10
+for i in range(10):
+   liste[i] = int(input())
+
+
+#Pour garder le plus petit des entiers, je le stocke dans la variable "intermediaire"...;;;   
+#Je mets un nombre très grand dans minimum pour que intermediaire soit de toute façon plus petit...mais...Nope. |'DDD
+
+minimum = 2 ** 20
+for i in range(10):
+   intermediaire = min(liste[0], liste[i])
+   if intermediaire < minimum:
+      minimum = intermediaire
+print(minimum)
