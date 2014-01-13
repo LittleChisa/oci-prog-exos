7c9035457a003171c9042b63ed74b92439508133
Chisa <monochromevoice@gmail.com>
2014-01-13 21:21:39 +0100

    Update 03d-analyse-de-frequence-obligatoire.py

--- niveau-02/chapitre-3-chaines-de-caracteres/03d-analyse-de-frequence-obligatoire.py
+++ niveau-02/chapitre-3-chaines-de-caracteres/03d-analyse-de-frequence-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 03d-analyse-de-frequence-obligatoire.py
 # nom de l'exercice : Analyse de fréquence
@@ -17,3 +16,18 @@
 
 # mettre votre code ici
 
+nb = input(). split(" ")
+nbLignes = nb[0]
+nbMots = nb[1]
+tableau = [0] * 101
+
+for loop in range(int(nbLignes)):
+   texte = input().split(" ")
+   
+   for i in range(int(nbMots)):
+      texte[i] = len(texte[i]) 
+      tableau[texte[i]] += 1
+      
+for i in range(101):
+   if tableau[i] > 0:
+      print(i, " : ", tableau[i])
