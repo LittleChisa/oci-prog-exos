7eb52e3bce3e60e064659d2dc95ee523094e8e4e
Chisa <monochromevoice@gmail.com>
2014-01-13 22:32:06 +0100

    Update 04f-ngms-sns-vlls-obligatoire.py

--- niveau-02/chapitre-3-chaines-de-caracteres/04f-ngms-sns-vlls-obligatoire.py
+++ niveau-02/chapitre-3-chaines-de-caracteres/04f-ngms-sns-vlls-obligatoire.py
@@ -1,4 +1,3 @@
-
 ##################################
 # fichier 04f-ngms-sns-vlls-obligatoire.py
 # nom de l'exercice : ngms sns vlls
@@ -17,3 +16,23 @@
 
 # mettre votre code ici
 
+titre = input().split(" ")
+auteur = input().split(" ")
+
+def sansVoyelle():
+   lettre = 0
+   for loop in range(len(liste)):
+      if liste[lettre] != "A" and liste[lettre] != "E" and liste[lettre] != "I" and liste[lettre] != "O" and liste[lettre] != "U" and liste[lettre] != "Y":
+         print(liste[lettre], end = "")
+      lettre += 1
+   lettre = 0
+
+for i in range(len(titre)):
+   liste = list(titre[i])
+   sansVoyelle()
+
+print("")
+
+for i in range(len(auteur)):
+   liste = list(auteur[i])
+   sansVoyelle()
